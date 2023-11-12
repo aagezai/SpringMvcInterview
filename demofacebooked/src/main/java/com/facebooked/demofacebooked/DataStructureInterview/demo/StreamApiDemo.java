9fe523d69d0bdf1a170c08bcbc915b6562646413
aagezai <EriAa@611252>
2023-11-12 15:23:35 -0600

    stream Api Demo added

---
+++ demofacebooked/src/main/java/com/facebooked/demofacebooked/DataStructureInterview/demo/StreamApiDemo.java
@@ -0,0 +1,61 @@
+package com.facebooked.demofacebooked.DataStructureInterview.demo;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class StreamApiDemo {
+    public static void main(String[] args) {
+        System.out.println("convert list of string to list of Characters");
+        List.of("AB $C", "DEF", "ABC")
+                .stream().flatMapToInt(str -> str.chars())
+                .mapToObj(ch -> (char) ch)
+                .sorted()
+                .collect(Collectors.toList()).forEach(System.out::print);
+        System.out.println("\n+++convert list of string to list of Characters and reverse order");
+        List.of("ABC", "DEFF", "GHIJ")
+                .stream().flatMap(str -> Arrays.stream(str.split("")))
+                .sorted(Comparator.reverseOrder())
+                .collect(Collectors.toList()).forEach(System.out::print);
+        System.out.println("\n+++++++ find non repeated numbers ");
+        List.of(5, 3, 6, 6, 2, 7, 7, 4, 1)
+                .stream().collect(Collectors.groupingBy(el -> el, Collectors.counting()))
+                .entrySet().stream()
+                .filter(el -> el.getValue() == 1)
+                .map(Map.Entry::getKey)
+                .collect(Collectors.toList())
+                .forEach(System.out::print);
+        System.out.println("\n +++++get only alphabets from list of words");
+        List.of("A@B", "CD#", "2E")
+                .stream().map(el -> el.replaceAll("[^a-zA-Z]", ""))
+                .collect(Collectors.toList()).forEach(System.out::print);
+        System.out.println("\n ++++++++or ---------");
+        List.of("A@B", "CD#", "2E").stream()
+                .flatMapToInt(el -> el.chars())
+                .mapToObj(ch -> (char) ch)
+                .filter(e -> Character.isLetter(e))
+                .collect(Collectors.toList())
+                .forEach(System.out::print);
+        System.out.println("\n+++++++++++ find the second and 3rd Employee by salary ");
+
+        List<Employee> employees = List.of(new Employee(121212L, "E1", 100000, "D1")
+                , new Employee(121212L, "E2", 110000, "D2")
+                , new Employee(121212L, "E3", 80000, "D3"));
+        employees.stream().
+                collect(Collectors.groupingBy(el -> Comparator.comparing(Employee::getSalary).reversed()))
+                .entrySet()
+                .stream()
+                .skip(1)
+                .limit(2)
+                .flatMap(entry -> entry.getValue().stream())
+                .collect(Collectors.groupingBy(Employee::getDepartment))
+                .forEach((department, departmentEmployees) -> {
+                    departmentEmployees.forEach(employee -> System.out.println("   " + employee));
+                    System.out.println();
+                });
+
+
+    }
+}
--- demofacebooked/src/main/java/com/facebooked/demofacebooked/DemofacebookedApplication.java
+++ demofacebooked/src/main/java/com/facebooked/demofacebooked/DemofacebookedApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemofacebookedApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(DemofacebookedApplication.class, args);
 	}
