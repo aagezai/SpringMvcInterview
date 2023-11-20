37af5a99a81f1d4933df342be1ed2755e3b426cb
aagezai <EriAa@611252>
2023-11-20 13:00:00 -0600

    changes in code

--- demofacebooked/src/main/java/com/facebooked/demofacebooked/DataStructureInterview/demo/StreamApiDemo.java
+++ demofacebooked/src/main/java/com/facebooked/demofacebooked/DataStructureInterview/demo/StreamApiDemo.java
@@ -40,26 +40,32 @@ public class StreamApiDemo {
 
         List<EmployeeUser> employees = List.of(new EmployeeUser(121212L, "E1", 100000, "D1")
                 , new EmployeeUser(121212L, "E2", 110000, "D2")
-                , new EmployeeUser(121212L, "E2", 80000, "D3")
-                , new EmployeeUser(2342424l, "aron", 90000, "D2"),
-                new EmployeeUser(345432l, "Biniam", 130000, "D3"));
+                , new EmployeeUser(121212L, "E0", 8000, "D3")
+                ,new EmployeeUser(233334l,"E5",110000,"D2")
+                ,new EmployeeUser(12144212L, "E6", 1000000, "D1"));
         System.out.println("+++++++ sort list of employee based on salary first then by department\n");
-        employees.stream().sorted(Comparator.comparing(EmployeeUser::getSalary).reversed().thenComparing(EmployeeUser::getDepartment))
-                .skip(1).limit(2).collect(Collectors.toList()).forEach(System.out::println);
-        System.out.println("++++++++++");
-        employees.stream().collect(Collectors.groupingBy(el -> el.getDepartment()))
+        employees.stream().sorted(Comparator.comparing(EmployeeUser::getSalary).reversed().thenComparing(EmployeeUser::getDepartment).thenComparing(EmployeeUser::getName))
+                /*.skip(1).limit(2)*/.collect(Collectors.toList()).forEach(System.out::println);
+        System.out.println("++++Group by Department  ++++++");
+       employees.stream().collect(Collectors.groupingBy(EmployeeUser::getDepartment))
                 .entrySet()
                 .stream()
-                .flatMap(el -> el.getValue().stream())
-                .sorted(Comparator.comparing(EmployeeUser::getSalary).reversed())
-                .skip(1)
-                .limit(2)
-                .collect(Collectors.groupingBy(EmployeeUser::getDepartment))
-                .forEach((de, em) -> {
-                            System.out.println(de + " " + em);
-                        }
+               .forEach(System.out::println);
+        System.out.println(" ++++++ Group by department and get EmployeeUser with max salary for each department");
+        Map<String, Optional<EmployeeUser>> maxSalaryEmployeesByDep = employees.stream()
+                .collect(Collectors.groupingBy(
+                        EmployeeUser::getDepartment,
+                        Collectors.maxBy(Comparator.comparingDouble(EmployeeUser::getSalary))
+                ));
+                          // Sort the result by max salary in descending order
+        maxSalaryEmployeesByDep.entrySet().stream()
+                .sorted((entry1, entry2) ->//(key,vale).stream()
+                        entry2.getValue().map(EmployeeUser::getSalary)
+                                .orElse(Double.MIN_VALUE)
+                                .compareTo(entry1.getValue().map(EmployeeUser::getSalary).orElse(Double.MIN_VALUE)))
+                .forEach(System.out::println);
+        System.out.println("---------------------------------");
 
-                );
         //sort it using lambda Comparator
         Comparator<EmployeeUser> comparator = (e1, e2) -> {
             int result;
@@ -77,5 +83,7 @@ public class StreamApiDemo {
         Comparator<EmployeeUser> comparator22 = Comparator.comparingDouble(EmployeeUser::getSalary);
         employees.stream().sorted(comparator22).collect(Collectors.toList())
                 .forEach(el -> System.out.println(el));
+        System.out.println("---------------------------------");
+
     }
 }
