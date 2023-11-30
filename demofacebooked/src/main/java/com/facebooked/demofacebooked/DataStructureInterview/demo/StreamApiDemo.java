ab8a28fb9ce18c932e36e4aeec9931f64f21dfe2
Biniam <109551743+biniammiu2022@users.noreply.github.com>
2023-11-30 14:47:50 -0800

    dfdj

--- demofacebooked/src/main/java/com/facebooked/demofacebooked/DataStructureInterview/demo/StreamApiDemo.java
+++ demofacebooked/src/main/java/com/facebooked/demofacebooked/DataStructureInterview/demo/StreamApiDemo.java
@@ -131,14 +131,8 @@ public class StreamApiDemo {
         EmployeeUser e2 = new EmployeeUser(22210L,"namee2",20000.00,"d2");
         List<EmployeeUser> employeeUserList = List.of(e2,e1);
         employeeUserList.stream().collect(Collectors.toMap(EmployeeUser::getId,EmployeeUser::getName)).forEach((a,b)-> System.out.println(a +" "+b));
-        System.out.println("test for git rebase");
         System.out.println("mainChanged");
         System.out.println("second manin change");
-        System.out.println("test for git rebase");
-        System.out.println();
-
-
-
 
     }
 }
