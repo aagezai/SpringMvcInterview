6a0573411ca0c918972374190baa72859159fe19
aagezai <EriAa@611252>
2023-12-02 17:07:42 -0600

    adde

---
+++ demofacebooked/src/main/java/com/facebooked/demofacebooked/AssociationHibernate/model/oneToOne/Bidir/OneToOneBiDirOwner.java
@@ -0,0 +1,14 @@
+package com.facebooked.demofacebooked.AssociationHibernate.model.oneToOne.Bidir;
+
+import jakarta.persistence.*;
+
+@Entity
+public class OneToOneBiDirOwner {//Address
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long OneToOneBiDirOwnerId;
+    private String OneToOneBiDirOwnerName;
+    @OneToOne
+    @JoinColumn(name = "oneToOneBiDir_id")
+    private OneToOneBiDir oneToOneBiDir;//Employee
+}
---
+++ demofacebooked/src/main/java/com/facebooked/demofacebooked/AssociationHibernate/model/oneToOne/OneToONeUniDir.java
@@ -0,0 +1,14 @@
+package com.facebooked.demofacebooked.AssociationHibernate.model.oneToOne;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class OneToONeUniDir {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id ;
+    private String OneToONeUniDirName;
+}
