5aaaf02c698d9af04b4dfa966c5990cc7d50db66
aagezai <EriAa@611252>
2023-12-03 14:41:24 -0600

    file added

---
+++ demofacebooked/src/main/java/com/facebooked/demofacebooked/AssociationHibernate/model/oneToMany/Bidir/OneToManyBiDirOwner.java
@@ -0,0 +1,16 @@
+package com.facebooked.demofacebooked.AssociationHibernate.model.oneToMany.Bidir;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "OneToManyBiDirOwnerAddress")
+public class OneToManyBiDirOwner {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long addressId;
+    private String addressName;
+    @ManyToOne
+    @JoinColumn(name = "employee id")
+    private OneToManyBiDir employeeBid;
+
+}
---
+++ demofacebooked/src/main/java/com/facebooked/demofacebooked/AssociationHibernate/model/oneToMany/OneToManyUniDir.java
@@ -0,0 +1,18 @@
+package com.facebooked.demofacebooked.AssociationHibernate.model.oneToMany;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "OneToManyUniDir_employee")
+public class OneToManyUniDir {//Employee
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long employeeId;
+    private String employeeName;
+    @OneToMany
+    @JoinColumn(name = "employee_ids")
+    private List<OneToManyUniOwner> addresses1;
+
+}
